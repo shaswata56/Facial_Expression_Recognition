a35404ed11688e310290e85fb348fd705d5cbc30
Shaswata Das <shaswata56@gmail.com>
2020-05-18 22:05:36 +0600

    trained again locally

--- camera.py
+++ camera.py
@@ -8,7 +8,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture("/home/rhyme/Desktop/Project/videos/facial_exp.mkv")
+        self.video = cv2.VideoCapture("videos/presidential_debate.mp4")
 
     def __del__(self):
         self.video.release()
--- model.py
+++ model.py
@@ -1,10 +1,9 @@
 from tensorflow.keras.models import model_from_json
 import numpy as np
 import tensorflow as tf
+gpus= tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
 
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.15
-session = tf.compat.v1.Session(config=config)
 
 class FacialExpressionModel(object):
     EMOTIONS_LIST = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
@@ -15,7 +14,7 @@ class FacialExpressionModel(object):
             self.loaded_model = model_from_json(loaded_model_json)
 
         self.loaded_model.load_weights(model_weights_file)
-        self.loaded_model._make_predict_function()
+        self.loaded_model.make_predict_function()
 
     def predict_emotion(self, img):
         self.preds = self.loaded_model.predict(img)
